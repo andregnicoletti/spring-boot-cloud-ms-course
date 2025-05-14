27054931fe83dd667680ceda0d51d62f5136cb21
André Nicoletti <andregnicoletti@gmail.com>
2025-05-14 11:50:30 -0300

    Database seeding

--- hr-user/src/main/java/com/nicoletti/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/nicoletti/hruser/HrUserApplication.java
@@ -1,15 +1,25 @@
 package com.nicoletti.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("BCRYPT=" + passwordEncoder.encode("123456"));
+	}
 }
--- hr-worker/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE tb_worker (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    daily_income DOUBLE
-);
