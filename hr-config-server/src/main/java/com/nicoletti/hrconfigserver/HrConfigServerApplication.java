9c154afe279bc7cb099e455c0223be39f27c7a50
André Nicoletti <andregnicoletti@gmail.com>
2025-05-15 11:58:03 -0300

    Adjusting propperties

--- hr-config-server/src/main/java/com/nicoletti/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/nicoletti/hrconfigserver/HrConfigServerApplication.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HrConfigServerApplication implements CommandLineRunner {
 
